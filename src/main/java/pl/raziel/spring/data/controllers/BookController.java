a110414ecee69f0dae012a421162fb7b96d956ff
go2osta <Diabolo!>
2017-08-21 12:22:11 +0200

    Derived Queries

---
+++ src/main/java/pl/raziel/spring/data/controllers/BookController.java
@@ -0,0 +1,64 @@
+package pl.raziel.spring.data.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.raziel.spring.data.entities.Book;
+import pl.raziel.spring.data.repositories.BookRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("books")
+public class BookController {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping
+    private ResponseEntity<List<Book>> getBooks() {
+        List<Book> books = bookRepository.findAll();
+
+        return new ResponseEntity<>(books, HttpStatus.OK);
+    }
+
+    @GetMapping("{title}")
+    private ResponseEntity<Book> getBook(@PathVariable("title") String title) {
+        Book byTitle = bookRepository.findByTitle(title);
+        System.out.println(byTitle);
+        if (byTitle == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(byTitle, HttpStatus.OK);
+    }
+
+    @GetMapping("bookByTitleLike")
+    private ResponseEntity<List<Book>> getBookByTitleLike(@PathVariable("title") String title) {
+
+        return new ResponseEntity<>(bookRepository.findByTitleLike(title), HttpStatus.OK);
+    }
+
+    @GetMapping("bookByTitleContaining")
+    private ResponseEntity<List<Book>> getBookByTitleContaining(@PathVariable("title") String title) {
+        return new ResponseEntity<>(bookRepository.findByTitleContaining(title), HttpStatus.OK);
+    }
+
+    @GetMapping("bookByTitleStartingWith")
+    private ResponseEntity<List<Book>> getBookByTitleStartingWith(@PathVariable("title") String title) {
+        return new ResponseEntity<>(bookRepository.findByTitleStartingWith(title), HttpStatus.OK);
+    }
+
+    @GetMapping("bookByTitleEndingWith")
+    private ResponseEntity<List<Book>> getBookByTitleEndingWith(@PathVariable("title") String title) {
+        return new ResponseEntity<>(bookRepository.findByTitleEndingWith(title), HttpStatus.OK);
+    }
+
+    @GetMapping("bookByTitleIgnoreCase")
+    private ResponseEntity<List<Book>> getBookByTitleIgnoreCase(@PathVariable("title") String title) {
+        return new ResponseEntity<>(bookRepository.findByTitleIgnoreCase(title), HttpStatus.OK);
+    }
+}
---
+++ src/main/java/pl/raziel/spring/data/repositories/BookRepository.java
@@ -0,0 +1,90 @@
+package pl.raziel.spring.data.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.raziel.spring.data.entities.Book;
+
+import java.util.Date;
+import java.util.List;
+
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+    /**
+     * String operators
+     */
+    Book findByTitle(String title);
+
+    List<Book> findByTitleLike(String title);
+
+    List<Book> findByTitleContaining(String title);
+
+    List<Book> findByTitleStartingWith(String title);
+
+    List<Book> findByTitleEndingWith(String title);
+
+    List<Book> findByTitleIgnoreCase(String title);
+
+    /**
+     * Relational operators
+     */
+
+    List<Book> findByPageCountEquals(int pageCount);
+
+    List<Book> findByPageCountGreaterThan(int pageCount);
+
+    List<Book> findByPageCountLessThan(int pageCount);
+
+    List<Book> findByPageCountGreaterThanEqual(int pageCount);
+
+    List<Book> findByPageCountLessThanEqual(int pageCount);
+
+    List<Book> findByPageCountBetween(int min, int max);
+
+    /**
+     * Logical operators
+     */
+
+    List<Book> findByTitleContainingOrTitleContaining(String title, String title2);
+
+    List<Book> findByTitleContainingAndPageCountGreaterThan(String title, int pageCount);
+
+    List<Book> findByTitleNot(String title);
+
+    /**
+     * Date comparison
+     */
+
+    List<Book> findByPublishDateAfter(Date date);
+
+    List<Book> findByPublishDateBefore(Date date);
+
+    List<Book> findByPublishDateBetween(Date date, Date date2);
+
+    /**
+     * Ordering results
+     */
+
+    List<Book> findByTitleContainingOrderByTitleAsc(String title);
+
+    List<Book> findByTitleContainingOrderByTitleDesc(String title);
+
+    List<Book> findTopByOrderByPageCountDesc();
+
+    List<Book> findFirstByOrderByPageCountAsc();
+
+    List<Book> findTop5ByOrderByPriceDesc();
+
+    List<Book> findTop5ByTitleOrderByPriceAsc(String title);
+
+    /**
+     * Transversing Nested Properties
+     */
+    List<Book> findByAuthorFirstName(String firstName);
+
+    List<Book> findByAuthor_Country(String country);
+
+
+}
+
+
